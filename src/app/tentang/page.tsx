e3207f2c9db3aec92c89dba63f15a5e86b0bafd3
markxplorer969 <markxplorer969@gmail.com>
2025-11-04 14:02:12 +0700

    Update otomatis 2025-11-04 14:02:12

--- src/app/tentang/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TentangPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-heading text-primary-text">Tentang Kami</h1>
-    </div>
-  );
-}
