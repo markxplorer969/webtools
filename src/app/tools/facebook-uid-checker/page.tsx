e683a4e9ef5777737c1fc71bc5972c350e4b7f26
markxplorer969 <markxplorer969@gmail.com>
2025-11-05 12:23:37 +0700

    Update otomatis 2025-11-05 12:23:37

--- src/app/tools/facebook-uid-checker/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { FacebookUidChecker } from '@/components/tools/FacebookUidChecker';
-
-export default function FacebookUIDCheckerPage() {
-  return <FacebookUidChecker />;
-}
